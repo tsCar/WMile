0f0e648c8428a050f553d4787b36a99e35013b77
tscar <tslavicekc@uniri.hr>
2015-12-28 15:47:35 +0100

    Promjena

--- WMile/src/Krtica.java
+++ WMile/src/Krtica.java
@@ -8,7 +8,7 @@ public class Krtica extends Button{
 	 */
 	private static final long serialVersionUID = 1L;
 	static Integer brojPogodaka=new Integer(0);
-	public Krtica(String string) {
-		super(string);
+	public Krtica(String da) {
+		super(da);
 	}
 }
