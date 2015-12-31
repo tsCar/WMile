acb941e4469eae84734c73a2265e530ef386aab2
tscar <tslavicekc@uniri.hr>
2015-12-31 14:56:04 +0100

    Dodan fail condition, kozmetičke promjene

--- WMile/src/Krtica.java
+++ WMile/src/Krtica.java
@@ -10,9 +10,11 @@ public class Krtica extends JButton{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	static Integer brojPogodaka=new Integer(0);
-	static Integer brojPojavljivanja=new Integer(1);
-	static Integer bodovi=new Integer(0);
+	static Integer brojPogodaka;//=new Integer();
+	static Integer brojPojavljivanja;//=new Integer(1);
+	static Integer bodovi;//=new Integer(0);
+	static Integer level;//=new Integer(0);
+	static Integer pogodakaOvajLevel;//=new Integer(0);
 	
 	public Krtica(String da) {
 		super(da);
--- WMile/src/Mile.java
+++ WMile/src/Mile.java
@@ -16,7 +16,7 @@ public class Mile extends JApplet implements ActionListener, MouseListener {
     Boolean kliknuto;
     JLabel label;
     Timer t;
-    int level=0,delay,sirinaSjene=30; //oprezno s ovim, množi se s konstantom u klasi Sjena a ne smije preć 254
+    int delay,sirinaSjene=30; //oprezno s ovim, množi se s konstantom u klasi Sjena a ne smije preć 254
     Sjena popupIzmeduLevela,popupVic;
     ImageIcon marina;
     JTextArea textZaPopupIzmeduLevela;
@@ -24,7 +24,11 @@ public class Mile extends JApplet implements ActionListener, MouseListener {
     
     @Override
     public void init() {
-    	 
+    	Krtica.bodovi=new Integer(0);
+    	Krtica.brojPogodaka=new Integer(0);
+    	Krtica.level=new Integer(0);
+    	Krtica.brojPojavljivanja=new Integer(1);
+    	Krtica.pogodakaOvajLevel=new Integer(0);//namjerno nije inicijalizirano u Krtici jer razmišljam o tome da usred igre možda dodam još jednu a ne želim da se vrijednosti izbrišu.   
         setLayout(null);
         backGround = getImage(getCodeBase(), "slika.jpg");
         label = new JLabel(new ImageIcon(backGround));
@@ -37,7 +41,7 @@ public class Mile extends JApplet implements ActionListener, MouseListener {
         b.setLocation(100, 100);
         b.setSize(111, 111); 
         kliknuto = false;
-        kolikoOdKoliko = new JTextField("tu \u0107e do\u0107 broj\u010di\u0107i");
+        kolikoOdKoliko = new JTextField(Krtica.brojPogodaka.toString()+" / "+Krtica.brojPojavljivanja+"    (level "+Krtica.level+")  "+delay);
         add(kolikoOdKoliko);
         kolikoOdKoliko.setHorizontalAlignment(JTextField.CENTER);
         kolikoOdKoliko.setLocation(getSize().width*3/8, 5);
@@ -85,7 +89,11 @@ public class Mile extends JApplet implements ActionListener, MouseListener {
     public void stop() {
     	Krtica.brojPogodaka=0;
     	Krtica.brojPojavljivanja=0;
-    	level=0;
+    	Krtica.level=0;
+    	popupIzmeduLevela.setEnabled(true);
+		popupIzmeduLevela.setVisible(true);
+		textZaPopupIzmeduLevela.setText("Joj, joj, joj, "+Krtica.pogodakaOvajLevel.toString()+"/10");
+		
     }
 
     @Override
@@ -95,37 +103,39 @@ public class Mile extends JApplet implements ActionListener, MouseListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
-			kolikoOdKoliko.setText(Krtica.brojPogodaka.toString()+" / "+Krtica.brojPojavljivanja+"    (level "+level+")  "+delay);
-			Krtica.brojPojavljivanja ++;
-			b.setLocation(ThreadLocalRandom.current().nextInt(0, getSize().width-b.getSize().width),ThreadLocalRandom.current().nextInt(0, getSize().height-b.getSize().height));//Znači, x na random od nula do (širina appleta-širina mileta), analogno za y
-			if(Krtica.brojPojavljivanja==4+10*level){
-				kliknuto = !kliknuto;
-				backGround = kliknuto ? getImage(getCodeBase(), "slika2.jpg") :getImage(getCodeBase(), "slika.jpg");
-				label.setIcon(new ImageIcon(backGround));
-				setContentPane(label);  //mijenja pozadinu appleta svaki level
-				t.stop();//zaustavi timer, da se ne pojavljuje Mile dok je aktivan popup
-				b.setEnabled(false);
-				b.setVisible(false);//sakrije i disable-a zadnjeg Mileta koji je bio nacrtan
-				level++;
-				delay=(int) ((delay>400) ? delay*0.95:400);
-				t.setInitialDelay(delay);
-				popupIzmeduLevela.setEnabled(true);
-				popupIzmeduLevela.setVisible(true);
-				if(level==1) {
-					switch (Krtica.brojPogodaka){
-					case 0:	textZaPopupIzmeduLevela.setText("Pa niti jednog? Realno, nije bilo teško, trebali ste moć...");break;
-					case 1: textZaPopupIzmeduLevela.setText("Ma da, kolega, pogodili ste jednoga ali to nije dovoljno. Nula bodova");break;
-					case 2: textZaPopupIzmeduLevela.setText("Ma, kolega, pogodili ste 2/3 ali nije to bilo dobro.\nOvog prvog niste potpuno, ovog drugog niste objasnili kako ste pogodili... \nNišta, 0 bodova");break;
-					case 3: textZaPopupIzmeduLevela.setText("Ma kolega, jeste Vi pogodili sva tri ali prvoga niste dovoljno precizno, drugog niste baš kako treba a ovog trećeg ste dobro ali ne onako kako sam ja zamislila. To će Vam bit 0 bodova.");break;
-					}
+		Krtica.brojPojavljivanja ++;
+		kolikoOdKoliko.setText(Krtica.brojPogodaka.toString()+" / "+Krtica.brojPojavljivanja+"    (level "+Krtica.level+")  "+delay);
+		b.setLocation(ThreadLocalRandom.current().nextInt(0, getSize().width-b.getSize().width),ThreadLocalRandom.current().nextInt(0, getSize().height-b.getSize().height));//Znači, x na random od nula do (širina appleta-širina mileta), analogno za y
+		if(Krtica.brojPojavljivanja==4+10*Krtica.level){
+			kolikoOdKoliko.setText("---");
+			Krtica.level++;
+			kliknuto = !kliknuto;
+			backGround = kliknuto ? getImage(getCodeBase(), "slika2.jpg") :getImage(getCodeBase(), "slika.jpg");
+			label.setIcon(new ImageIcon(backGround));
+			setContentPane(label);  //mijenja pozadinu appleta svaki level
+			t.stop();//zaustavi timer, da se ne pojavljuje Mile dok je aktivan popup
+			b.setEnabled(false);
+			b.setVisible(false);//sakrije i disable-a zadnjeg Mileta koji je bio nacrtan
+			delay=(int) ((delay>400) ? delay*0.95:400);
+			t.setInitialDelay(delay);
+			popupIzmeduLevela.setEnabled(true);
+			popupIzmeduLevela.setVisible(true);
+			if(Krtica.level==1) {
+				switch (Krtica.brojPogodaka){
+				case 0:	textZaPopupIzmeduLevela.setText("Pa niti jednog? Realno, nije bilo teško, trebali ste moć...");break;
+				case 1: textZaPopupIzmeduLevela.setText("Ma da, kolega, pogodili ste jednoga ali to nije dovoljno. Nula bodova");break;
+				case 2: textZaPopupIzmeduLevela.setText("Ma, kolega, pogodili ste 2/3 ali nije to bilo dobro.\nOvog prvog niste potpuno, ovog drugog niste objasnili kako ste pogodili... \nNišta, 0 bodova");break;
+				case 3: textZaPopupIzmeduLevela.setText("Ma kolega, jeste Vi pogodili sva tri ali prvoga niste dovoljno precizno, drugog niste baš kako treba a ovog trećeg ste dobro ali ne onako kako sam ja zamislila. To će Vam bit 0 bodova.");break;
 				}
+			}
+			else{
+				if (Krtica.pogodakaOvajLevel<5)stop();
 				else{
 					textZaPopupIzmeduLevela.setText(Krtica.bodovi.toString());
+					}
 				}
-				
 			}
 			else t.restart();
-		
 	}
   
 
@@ -147,8 +157,9 @@ public class Mile extends JApplet implements ActionListener, MouseListener {
 	public void mousePressed(MouseEvent e) {
 		if(e.getSource()==b){  
 			Krtica.brojPogodaka ++;
-			Krtica.bodovi+=level;
-			kolikoOdKoliko.setText(Krtica.brojPogodaka.toString()+" / "+Krtica.brojPojavljivanja+"    (level "+level+")");
+			Krtica.bodovi+=Krtica.level;
+			Krtica.pogodakaOvajLevel ++;
+			kolikoOdKoliko.setText(Krtica.brojPogodaka.toString()+" / "+Krtica.brojPojavljivanja+"    (level "+Krtica.level+")");
 			bodovi.setText(Krtica.bodovi.toString());
 		}
 	}
